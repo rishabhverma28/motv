47be64990ae31c5355849bad30bd4e1231f660be
Rishabh Verma <rishabhverma.2806@gmail.com>
2019-08-15 16:10:38 +1000

    Updated Router

--- src/App.js
+++ src/App.js
@@ -2,37 +2,24 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 // import homepageSVG from "./Images/combined.svg";
-import SiteImage from "./Components/SiteImage/SiteImage";
 import Homepage from "./Components/Homepage/Homepage";
 import TitleInfo from "./Components/TitleInfo/TitleInfo";
-import { Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <Switch>
-                {/* <SiteImage /> */}
-                <Route exact path="/" component={Homepage} />
-                <Route exact path="/title/:id" component={TitleInfo} />
-                {/* <img src={homepageSVG} alt="site intro" className="site-image" /> */}
-                {/* <Searcher /> */}
-            </Switch>
-        </div>
-        //     <header className="App-header">
-        //         <img src={logo} className="App-logo" alt="logo" />
-        //         <p>
-        //             Edit <code>src/App.js</code> and save to reload.
-        //         </p>
-        //         <a
-        //             className="App-link"
-        //             href="https://reactjs.org"
-        //             target="_blank"
-        //             rel="noopener noreferrer"
-        //         >
-        //             Learn React
-        //         </a>
-        //     </header>
+        <HashRouter basename="/">
+            <div className="App">
+                <Header />
+                <Switch>
+                    {/* <SiteImage /> */}
+                    <Route exact path="/" component={Homepage} />
+                    <Route exact path="/title/:id" component={TitleInfo} />
+                    {/* <img src={homepageSVG} alt="site intro" className="site-image" /> */}
+                    {/* <Searcher /> */}
+                </Switch>
+            </div>
+        </HashRouter>
     );
 }
 

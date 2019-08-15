9566803704cae47620fa35b8e25b10f585ebea68
Rishabh Verma <rishabhverma.2806@gmail.com>
2019-08-15 15:55:24 +1000

    Updates

---
+++ precache-manifest.e873609b1b9e4e89d34b9ff61aa74951.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbbec5a73f9aeb398ff7f7a1d929745",
+    "url": "/motv/index.html"
+  },
+  {
+    "revision": "9ddf86e3b30ff2c928c5",
+    "url": "/motv/static/css/main.78ca0dfc.chunk.css"
+  },
+  {
+    "revision": "b90ccb432e11335bfe61",
+    "url": "/motv/static/js/2.26c14318.chunk.js"
+  },
+  {
+    "revision": "9ddf86e3b30ff2c928c5",
+    "url": "/motv/static/js/main.bcf6bd6b.chunk.js"
+  },
+  {
+    "revision": "0f77d0e37822a05aa193",
+    "url": "/motv/static/js/runtime~main.91f531ec.js"
+  },
+  {
+    "revision": "070671669585bc88f17f5db67aa410c0",
+    "url": "/motv/static/media/combined.07067166.svg"
+  },
+  {
+    "revision": "5487724f32904483285697ef0fac3f1b",
+    "url": "/motv/static/media/motv.5487724f.svg"
+  }
+]);
